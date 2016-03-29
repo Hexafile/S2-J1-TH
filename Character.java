dd546e4879681aec5e073b0a6e491b52b4a5d765
Swits <robin62840@gmail.com>
2016-03-29 11:30:52 +0200

    changement ajouté en cours

--- Character.java
+++ Character.java
@@ -1,5 +1,5 @@
 package org.treasurehunt.character;
-import org.treasurehunt.plateau.*;
+import org.treasurehunt.plateau.Board;
 
 public class Character {
 	private int team; 
@@ -40,17 +40,39 @@ public boolean[][] getMoves () {
 	boolean[][] moves = new boolean[3][3];
 	for (int i=0; i<3; i++ ) {
 		for (int j=0; j<3; j++) {
-			if (Board.getBoard()[x-1+i][y-1+1].getReachable()) moves[i][j]=true;
+			if (Board.getBoard()[x-1+i][y-1+j].getReachable()) moves[i][j]=true;
 		}
 	}
 	return moves;
 }
 
 public void move (int x, int y) {
-	Board.getBoard()[this.y][this.x].setCharacter(null);;
+	this.setVisible(true);
+	Board.getBoard()[this.y][this.y].setReachable(true);
+	Board.getBoard()[this.y][this.x].setCharacter(null);
 	this.x=x;
 	this.y=y;
-	Board.getBoard()[x][y].setCharacter(this); // Je vois pas pourquoi ça marche pas...
+	Board.getBoard()[x][y].setCharacter(this);
+	Board.getBoard()[this.y][this.y].setReachable(false);
+	this.setVisible(false);
+}
+
+public void setVisible(boolean clean) {
+	if (clean) {
+		for (int i=0; i<5; i++) {
+			for (int j=0; j<5; j++) {
+				if (Math.sqrt( (double) ((i-this.getX()) * (i-this.getX()) + (j-this.getY())*(j-getY() ))) < 3.0 )
+				Board.getBoard()[y][x].setVisible(Board.getBoard()[y][x].getVisible()-this.team);
+			}
+		}
+	} else {
+		for (int i=0; i<5; i++) {
+			for (int j=0; j<5; j++) {
+				if (Math.sqrt( (double) ((i-this.getX()) * (i-this.getX()) + (j-this.getY())*(j-getY() ))) < 3.0 )
+				Board.getBoard()[y][x].setVisible(this.team);
+			}
+		}
+	}
 }
 }
 
