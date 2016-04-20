5f7fd389e5702c3e9c2a436c100bd7e6c1b03ad4
MaximeGmpz <max63710@hotmail.fr>
2016-04-20 19:00:43 +0200

    Ajout class Constant

---
+++ src/org/treasurehunt/plateau/Constant.java
@@ -0,0 +1,34 @@
+package org.treasurehunt.plateau;
+
+public final class Constant {
+	
+	/* Constants for characters */
+	
+	public final static int EXPLORER = 1;
+	
+	public final static int THIEF = 2;
+	
+	public final static int SCAVENGER = 3;
+	
+	public final static int WARRIOR = 4;
+	
+	/* Constants for cells */
+	
+	public final static int GROUND = 0;
+	
+	public final static int ROCK = 1;
+	
+	public final static int KEY = 2;
+	
+	public final static int CHEST = 3;
+	
+	public final static int WATER = 4;
+	
+	/* Constants for team base */
+	
+	public final static int NOBASE = 0;
+	
+	public final static int TEAM1 = 1;
+	
+	public final static int TEAM2 = 2;
+}
